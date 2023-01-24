21a6cce83c9c09387bd0c4b38f9300e89bc2efe1
GustavTP <gustavtp1999@gmail.com>
2023-01-24 12:35:54 -0500

    cruds

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => 'bjjoyz0txpt4hittervices.clever-cloud.com',
+    'user' => 'umqmfqs',
+    'pass' => 'umqmfqqol0',
+    'name' => 'bjjoyz0tkznp',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
